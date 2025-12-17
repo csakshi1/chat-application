5f1618f6faef843df1f9ffbd10e1aff969e27a92
Sakshi Choudhary <csakshi1@gmail.com>
2025-12-17 13:16:02 +0530

    Fix BASE_URL using VITE env variable

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -2,8 +2,7 @@ import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import axios from "axios";
 import toast from "react-hot-toast";
-import { BASE_URL } from '../config';
-
+import { BASE_URL } from '..';
 
 
 const Signup = () => {
